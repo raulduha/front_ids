ac71a7fce81ea615dc93a0dc822addc37306b6b7
raulduha <80332171+raulduha@users.noreply.github.com>
2023-10-03 10:30:54 -0300

    navbar

---
+++ client/src/pages/HomePage.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function HomePage() {
+  return (
+    <div className="home-page">
+      <h2>Bienvenido al homepage</h2>
+      {/* Add any content or features you want for the homepage */}
+    </div>
+  );
+}
+
+export default HomePage;
