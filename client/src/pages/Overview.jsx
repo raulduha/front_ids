369974de0a0dc86369b0eb16902d863b1c6e5229
raulduha <80332171+raulduha@users.noreply.github.com>
2023-11-08 12:31:23 -0300

    overview filtro fecha

--- client/src/pages/Overview.jsx
+++ client/src/pages/Overview.jsx
@@ -1,11 +1,15 @@
+// Overview.js
+
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
+import moment from 'moment'; // Make sure to install moment via npm or yarn
 import './Overview.css';
 
 const Overview = () => {
   const [products, setProducts] = useState([]);
   const [productionRecords, setProductionRecords] = useState([]);
   const [storageRecords, setStorageRecords] = useState([]);
+  const [filter, setFilter] = useState('all'); // State to control the active filter
 
   const baseURL = 'http://localhost:8000/records/api/v1';
 
@@ -22,36 +26,59 @@ const Overview = () => {
         setProductionRecords(productionResponse.data);
         setStorageRecords(storageResponse.data);
       } catch (error) {
-        // Handle error for each request if needed
         console.error('Error fetching data:', error);
-        // Optionally set state to show error messages in the UI
       }
     };
 
     fetchData();
   }, []);
 
+  const filteredProductionRecords = (productID) => {
+    const today = moment().startOf('day');
+    return productionRecords
+      .filter(record => record.product_id === productID)
+      .filter(record => {
+        const recordDate = moment(record.date_created);
+        switch (filter) {
+          case 'today':
+            return recordDate.isSame(today, 'day');
+          case 'week':
+            return recordDate.isSame(today, 'week');
+          case 'month':
+            return recordDate.isSame(today, 'month');
+          case 'all':
+          default:
+            return true;
+        }
+      });
+  };
+
+  const calculateTotal = (records) => {
+    return records.reduce((acc, record) => acc + Number(record.amount), 0);
+  };
+
   return (
     <div className="overview">
       <h1>Comparación de Productos</h1>
+      <div className="filter-buttons">
+        <button onClick={() => setFilter('today')}>Hoy</button>
+        <button onClick={() => setFilter('week')}>Semana</button>
+        <button onClick={() => setFilter('month')}>Mes</button>
+        <button onClick={() => setFilter('all')}>Todos</button>
+      </div>
       <table className="overview-table">
         <thead>
           <tr>
             <th>ID Producto</th>
             <th>Nombre Producto</th>
-            <th>Producción Total</th>
+            <th>Producción {filter === 'all' ? '' : `(${filter})`}</th>
             <th>Almacenamiento Total</th>
           </tr>
         </thead>
         <tbody>
           {products.map((product) => {
-            const totalProduction = productionRecords
-              .filter(record => record.product_id === product.product_id)
-              .reduce((acc, record) => acc + Number(record.amount), 0);
-
-            const totalStorage = storageRecords
-              .filter(record => record.product_id === product.product_id)
-              .reduce((acc, record) => acc + Number(record.amount), 0);
+            const totalProduction = calculateTotal(filteredProductionRecords(product.product_id));
+            const totalStorage = calculateTotal(storageRecords.filter(record => record.product_id === product.product_id));
 
             return (
               <tr key={product.product_id}>
