62bbc65d56b844b8047cf5ea0cddbcad8658bd5c
raulduha <80332171+raulduha@users.noreply.github.com>
2023-11-16 00:17:39 -0300

    cambiamos host 3

--- client/src/api/users.api.js
+++ client/src/api/users.api.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
 
 export const getAllUsers = () => {
-    return axios.get("http://localhost:8000/records/api/v1/users/")
+    return axios.get("http://24.144.85.42:8001/records/api/v1/users/")
 }
